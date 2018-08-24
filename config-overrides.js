2d57f8e875014dc35fdd313a58e7925b6766f0cb
weifuchuan <fuchuan.wei@hotmail.com>
2018-08-24 23:15:48 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
 module.exports = function override(config, env) {
-  config.output.publicPath = "https://cdn.jsdelivr.net/gh/weifuchuan/fuchuansia-web@1.0/build/"; 
+  config.output.publicPath = "https://cdn.jsdelivr.net/gh/weifuchuan/fuchuansia-web@1.1/build/"; 
   return config;
 };
